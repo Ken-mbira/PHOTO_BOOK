05f0fcd7aa09805eaa0215e7fb1ce4079e31db80
ken <ken.mbira@student.moringaschool.com>
2021-10-06 19:39:55 +0300

    add location and categoru tables

--- photo/models.py
+++ photo/models.py
@@ -8,23 +8,23 @@ class Category(models.Model):
     """ 
     name = models.CharField(max_length=50)
 
-# class Location(models.Model):
-#     """This defines the location table and all its contents and behaviours
+class Location(models.Model):
+    """This defines the location table and all its contents and behaviours
 
-#     Args:
-#         models ([module]): [This is where we import the model functionality]
-#     """ 
-#     name = models.CharField()
+    Args:
+        models ([module]): [This is where we import the model functionality]
+    """ 
+    name = models.CharField(max_length=50)
 
-# class Image(models.Model):
-#     """This defines the image table and all its contents and behaviours
+class Image(models.Model):
+    """This defines the image table and all its contents and behaviours
 
-#     Args:
-#         models ([module]): [This is where we import the model functionality]
-#     """ 
-#     name = models.CharField()
-#     image_path = models.TextField(upload_to = 'articles/')
-#     date_taken = models.DateTimeField()
-#     descriptions = models.TextField(blank=True)
-#     category = models.ForeignKey(Category)
-#     location = models.ForeignKey(Location)
+    Args:
+        models ([module]): [This is where we import the model functionality]
+    """ 
+    name = models.CharField(max_length=50)
+    # image_path = models.TextField(upload_to = 'articles/')
+    date_taken = models.DateTimeField()
+    descriptions = models.TextField(blank=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
