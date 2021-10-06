31ce109ec73cd7b80c4d70fa1330303020c6cb6a
ken <ken.mbira@student.moringaschool.com>
2021-10-06 19:36:49 +0300

    created a categories table

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    """This defines the category table and all its contents and behaviours
+
+    Args:
+        models ([module]): [This is where we import the model functionality]
+    """ 
+    name = models.CharField(max_length=50)
+
+# class Location(models.Model):
+#     """This defines the location table and all its contents and behaviours
+
+#     Args:
+#         models ([module]): [This is where we import the model functionality]
+#     """ 
+#     name = models.CharField()
+
+# class Image(models.Model):
+#     """This defines the image table and all its contents and behaviours
+
+#     Args:
+#         models ([module]): [This is where we import the model functionality]
+#     """ 
+#     name = models.CharField()
+#     image_path = models.TextField(upload_to = 'articles/')
+#     date_taken = models.DateTimeField()
+#     descriptions = models.TextField(blank=True)
+#     category = models.ForeignKey(Category)
+#     location = models.ForeignKey(Location)
