94e674dd7eaca561706f0ec6b64941a3a200311f
ken <ken.mbira@student.moringaschool.com>
2021-10-07 05:12:27 +0300

    add tests for the category class

--- photo/tests.py
+++ photo/tests.py
@@ -18,9 +18,26 @@ class TestImage(TestCase):
         """
         self.assertTrue(isinstance(self.graduation,Image))
 
-    def test_save_image(self):
-        """This tests whether an image can be saved to the database
+    # def test_save_image(self):
+    #     """This tests whether an image can be saved to the database
+    #     """
+    #     self.graduation.save_image()
+    #     images = Image.objects.all()
+    #     self.AssertTrue(len(images) > 1)
+
+
+class TestCategory(TestCase):
+    """This defines tests for all behaviours of the Category class
+
+    Args:
+        TestCase ([type]): [description]
+    """
+    def setUp(self):
+        """This will run before every test does
         """
-        self.graduation.save_image()
-        images = Image.objects.all()
-        self.AssertTrue(len(images) > 1)
+        self.category1 = Category(name = 'mycategory',description = 'This is my category')
+
+    def test_category_instance(self):
+        """This will test if the the category is an instance of the category class
+        """
+        self.assertTrue(isinstance(self.category1,Category))
