8cdab2de39c9abab408aeb2427109129ecc6a953
ken <ken.mbira@student.moringaschool.com>
2021-10-10 14:06:43 +0300

    added a category filter for the images

--- photo/urls.py
+++ photo/urls.py
@@ -8,7 +8,8 @@ from . import views
 urlpatterns = [
     path('',views.index,name = 'home'),
     path('images/',views.images,name = 'images'),
-    path('images/<int:pk>',views.image_spec,name = 'image')
+    path('images/<int:pk>',views.image_spec,name = 'image'),
+    path('category/<int:pk>',views.image_category,name = 'category')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- photo/views.py
+++ photo/views.py
@@ -10,7 +10,8 @@ from django.core import serializers
 def index(request):
     """This renders the home page
     """
-    return render(request,'photohtml/index.html')
+    categories = Category.objects.all()
+    return render(request,'photohtml/index.html',{"categories":categories})
 
 def images(request):
     """This will render the images page
@@ -18,8 +19,9 @@ def images(request):
     Args:
         request ([type]): [description]
     """
+    categories = Category.objects.all()
     images = Image.objects.all()
-    return render(request,'photohtml/images.html',{'images':images})
+    return render(request,'photohtml/images.html',{'images':images,"categories":categories})
 
 def image_spec(request,pk):
     """This will render the page containing a specific image
@@ -28,14 +30,18 @@ def image_spec(request,pk):
         request ([type]): [description]
         pk ([int]): [This is the primary key of the image]
     """
+    categories = Category.objects.all()
     image = Image.get_image_by_id(pk)
-    return render(request,"photohtml/image.html",{"image":image})
+    return render(request,"photohtml/image.html",{"image":image,"categories":categories})
 
-# def image_category(request,pk):
-#     """This will render a page containing the specific category asked for
+def image_category(request,pk):
+    """This will render a page containing the specific category asked for
 
-#     Args:
-#         request ([type]): [description]
-#         pk ([type]): [description]
-#     """
-#     images = Image.get_image_by_category()
+    Args:
+        request ([type]): [description]
+        pk ([type]): [description]
+    """
+    categories = Category.objects.all()
+    images = Image.get_image_by_category(pk)
+
+    return render(request,'photohtml/image_category.html',{"images":images,"categories":categories})
