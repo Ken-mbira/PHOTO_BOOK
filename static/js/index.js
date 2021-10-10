e3d6d8549ac7fef7043ed02ed035217c5f54b958
ken <ken.mbira@student.moringaschool.com>
2021-10-10 14:39:47 +0300

    add a feature to show the image details immediately one logs on to the image page

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-    $(".image").click(function(e) {
-        var myModal = new bootstrap.Modal(document.getElementById('gallery-modal'))
-        myModal.show()
-    })
+    var myModal = new bootstrap.Modal(document.getElementById('gallery-modal'))
+    myModal.show()
 })
