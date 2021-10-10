c5319da86d744d0bbf73780ac7d23eba69a9ddea
ken <ken.mbira@student.moringaschool.com>
2021-10-10 13:28:49 +0300

    add the categories to the navbar

--- photo/views.py
+++ photo/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 from .forms import ImageForm
-from .models import Image
+from .models import Image,Category
 from django.core import serializers
 
 
 def index(request):
     """This renders the home page
     """
-    return render(request,'photohtml/index.html') 
+    return render(request,'photohtml/index.html')
 
 def images(request):
     """This will render the images page
@@ -19,9 +19,7 @@ def images(request):
         request ([type]): [description]
     """
     images = Image.objects.all()
-    dataholder = serializers.serialize("json",images)
-    data = json.dumps(dataholder)
-    return render(request,'photohtml/images.html',{'images':images,"data":data})
+    return render(request,'photohtml/images.html',{'images':images})
 
 def image_spec(request,pk):
     """This will render the page containing a specific image
