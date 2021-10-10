aeea72ed7d3f299a889df13fe5d908344f92a4dc
ken <ken.mbira@student.moringaschool.com>
2021-10-10 12:59:13 +0300

    add a modal to display the image descriptions

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,6 @@
-// $(document).ready(function() {
-//     $(".image").click(function(e) {
-//         const src = e.target.getAttribute('src');
-//         const data = e.target.getAttribute('value');
-//         const pk = e.target.getAttribute('id');
-//         const data2 = JSON.parse(JSON.parse(data))
-//         document.querySelector(".modal-img").src = src;
-//         var myModal = new bootstrap.Modal(document.getElementById('gallery-modal'))
-//         myModal.show()
-//         $(".modal-title").html(data2[pk].fields.name)
-//         $("#image-description").html(data2[pk].fields.descriptions)
-//         $("#image-category").html(data2[pk].fields.category)
-//         $("#date-taken").html(data2[pk].fields.date_taken)
-//     })
-// })
+$(document).ready(function() {
+    $(".image").click(function(e) {
+        var myModal = new bootstrap.Modal(document.getElementById('gallery-modal'))
+        myModal.show()
+    })
+})
