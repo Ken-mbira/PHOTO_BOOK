5db56a7ed44cc4cf2c5cd50eacbcec2c290a158b
ken <ken.mbira@student.moringaschool.com>
2021-10-11 10:36:54 +0300

    implement the copy to clipboard functionality with javascript

--- static/js/index.js
+++ static/js/index.js
@@ -5,4 +5,10 @@ $(document).ready(function() {
         var myModal = new bootstrap.Modal(document.getElementById('gallery-modal'))
         myModal.show()
     })
+    $("#copy-link").click(function(e) {
+        var message = e.target
+        navigator.clipboard.writeText(message.value)
+        var otherModal = new bootstrap.Modal(document.getElementById('success-modal'))
+        otherModal.show()
+    })
 })
