17111d0aee40f53a5d18c0fe3e7a1e8eeb261edc
ken <ken.mbira@student.moringaschool.com>
2021-10-11 13:35:28 +0300

    set up local database

--- photobook/settings.py
+++ photobook/settings.py
@@ -82,8 +82,10 @@ WSGI_APPLICATION = 'photobook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photobook',
+        'USER': 'kenmbira',
+        'PASSWORD':'1234',
     }
 }
 
